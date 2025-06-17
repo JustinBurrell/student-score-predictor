a06d4a0910eabb4fa29f0d67dc96271fadc583c6
Justin Burrell <justinburrell715@gmail.com>
2025-06-17 02:56:40 -0400

    Working on fixing virtual environment

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/justinburrell/Desktop/Personal CS Projects/ml-predictor-app/.venv"
+set -gx VIRTUAL_ENV "/Users/justinburrell/Desktop/Personal CS Projects/student-score-predictor/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
